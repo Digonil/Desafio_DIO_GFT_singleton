65cca06a7a1cb3ba54ddef81096c089a04dbe153
Digonil <diego.niles@hotmail.com>
2022-03-09 20:21:22 -0300

    Aplicação de Singleton

---
+++ src/one/digitalinnovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.gof;
+
+// Declaracoes Static nao suportadas na versao 11 do java
+
+//public class SingletonLazyHolder {
+//
+//
+//    public class SingletonLazyHolder {
+//
+//        private static class InstanceHolder {
+//            public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+//        }
+//
+//        private SingletonLazyHolder() {
+//            super();
+//        }
+//
+//        public static SingletonLazyHolder getInstancia() {
+//            return InstanceHolder.instancia;
+//        }
+//    }
+//}
